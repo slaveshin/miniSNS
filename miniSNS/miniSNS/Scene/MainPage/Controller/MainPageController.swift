4911b9c544e66780c266461c66ad1fe5e3aca4da
shin seung jae <shinseungjae1234@gmail.com>
2022-10-03 19:03:52 +0900

    add: 기본 파일 추가

---
+++ miniSNS/miniSNS/Scene/MainPage/Controller/MainPageController.swift
@@ -0,0 +1,8 @@
+//
+//  MainPageController.swift
+//  miniSNS
+//
+//  Created by 신승재 on 2022/10/03.
+//
+
+import Foundation
---
+++ miniSNS/miniSNS/Scene/ProfilePage/Controller/ProfilePageController.swift
@@ -0,0 +1,8 @@
+//
+//  Controller'.swift
+//  miniSNS
+//
+//  Created by 신승재 on 2022/10/03.
+//
+
+import Foundation
